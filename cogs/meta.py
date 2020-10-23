b479e94f8bc8038af00edb354249ec328ed3e9ee
Tim Robinson <timrobin@gmail.com>
2020-10-22 23:11:20 -0400

    remove ad and +invite

--- cogs/meta.py
+++ cogs/meta.py
@@ -9,7 +9,7 @@ class Meta(commands.Cog):
     @commands.command(name="help")
     async def help(self, ctx):
         emb1 = discord.Embed(
-            description="**Reaction Poll**\nCreate a reaction poll by typing `+poll *your message*`. Poll Bot will automatically add the reactions 👍, 👎, and 🤷.\nCreate a reaction poll with multiple options by typing `+poll {title} [Option1] [Option2] [Option3]`.\n\n**Strawpoll**\nCreate a strawpoll by typing `+strawpoll {title} [Option1] [Option2] [Option 3]`, with up to 30 options.\n\n**Other Commands**\n+invite, +donate\n\n**Still Have Questions?**\nJoin our Discord server: <https://discord.gg/FhT6nUn>"
+            description="**Reaction Poll**\nCreate a reaction poll by typing `+poll *your message*`. Poll Bot will automatically add the reactions 👍, 👎, and 🤷.\nCreate a reaction poll with multiple options by typing `+poll {title} [Option1] [Option2] [Option3]`.\n\n**Strawpoll**\nCreate a strawpoll by typing `+strawpoll {title} [Option1] [Option2] [Option 3]`, with up to 30 options.\n\n**Other Commands**\n+donate\n\n**Still Have Questions?**\nJoin our Discord server: <https://discord.gg/FhT6nUn>"
             + "\n"
             + "Ask us on Twitter: <https://twitter.com/DiscordPollBot>",
             colour=0x83BAE3,
@@ -26,14 +26,6 @@ class Meta(commands.Cog):
                         + "Bitcoin Address: 3EPBikSQ9MTsHrnowLvPfe83uYZ4bFvohM\n"
                         + "Bitcoin (via Lightning Network): https://tippin.me/@stayingqold\n"
                         + "Ether: 0x00C78c405B21c120c921991906A61A52A1D1DC71",
-            colour=0x83BAE3,
-        )
-        await ctx.message.channel.send(embed=emb1)
-
-    @commands.command(name="invite")
-    async def invite(self, ctx):
-        emb1 = discord.Embed(
-            description="Invite Poll Bot to your server: <https://discordapp.com/oauth2/authorize?client_id=298673420181438465&scope=bot&permissions=0>",
             colour=0x83BAE3,
         )
         await ctx.message.channel.send(embed=emb1)
